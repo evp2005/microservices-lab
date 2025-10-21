7988757f2a8b9c0c98d6ef9aa0d890c3b15ed723
Evor Vega <152338343+evp2005@users.noreply.github.com>
2025-10-21 12:57:43 -0500

    Final Proyecto

--- auth-service/test_connection.py
+++ auth-service/test_connection.py
@@ -1,21 +1,44 @@
+import redis
 import psycopg2
 
-connection_db = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="main_db",
-    user="devuser",
-    password="devpass" 
-)
+host_db="localhost"
+port_db=5432
+database_db="main_db"
+user_db="devuser"
+password_db="devpass" 
 
-# Creamos un cursor para ejecutar consultas
-cursor_con = connection_db.cursor()
+def verify_connection(host,port,database,user,password):
+    connection_db = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password 
+    )
 
-# Probar la conexión
-cursor_con.execute("SELECT version();")
-version = cursor_con.fetchone()
-print("Conectado a:", version)
+    # Creamos un cursor para ejecutar consultas
+    cursor_con = connection_db.cursor()
 
-# Cerrar la conexión
-cursor_con.close()
-connection_db.close()
+    # Probar la conexión
+    cursor_con.execute("SELECT version();")
+    version = cursor_con.fetchone()
+    result = f"Conectado a: {version}"
+
+    # Cerrar la conexión
+    cursor_con.close()
+    connection_db.close()
+    return print(result)
+
+def test_redis(host, username):
+    # Conectar al servicio Redis que corre en tu Docker
+    r = redis.Redis(host=host, port=6379, db=0)
+
+    # Guardar un valor
+    r.set('usuario', username)
+
+    # Recuperarlo
+    return print(r.get('usuario'))  # b'Evor'
+    
+verify_connection("localhost",5432,"main_db","devuser","devpass")
+test_redis("localhost", username="Franco")
+
